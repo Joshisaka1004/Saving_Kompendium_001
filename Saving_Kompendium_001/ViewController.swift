b6b2dd5664ea5cf107630f8a5a94f80f1acb457c
Joshisaka1004 <joshi1004@gmail.com>
2019-01-08 19:52:44 +0100

    Initial Commit

---
+++ Saving_Kompendium_001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saving_Kompendium_001
+//
+//  Created by Joachim Vetter on 08.01.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
